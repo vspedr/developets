bdf55c9d61b9ca8e642a1a7209d814674c2b0631
vspedr <vspedrazzi@gmail.com>
2017-12-08 13:46:55 -0200

    Simplify PetCard component and add placeholder images

--- src/components/PetCard.js
+++ src/components/PetCard.js
@@ -1,29 +1,25 @@
 import React from 'react';
 import { Card, Icon } from 'semantic-ui-react';
+import placeholder from '../assets/img/paw.png';
 
-export const PetCard = (pet, index) => (
-  <Card key={index}>
-    <Card.Content>
-      <Card.Header>
-        {pet.name}
-      </Card.Header>
-      <Card.Meta>
-        <span className='age'>
-          { pet.subtype ?
-            `the ${pet.subtype} ${pet.type}` :
-            `the ${pet.type}`
-          }
-        </span>
-      </Card.Meta>
-      <Card.Description>
-        {pet.description}
-      </Card.Description>
-      <Card.Content extra>
-        <a href={`http://github.com/${pet.owner}`}>
-          <Icon name='user' />
-          {`Owner: ${pet.owner}`}
-        </a>
-      </Card.Content>
+export const PetCard = (pet, index) => {
+  const extra = (
+    <Card.Content extra>
+      <a href={`http://github.com/${pet.owner}`}>
+        <Icon name='github' />
+        {pet.owner}
+      </a>
     </Card.Content>
-  </Card>
-);
+  );
+
+  return (
+    <Card
+      key={index}
+      image={pet.img || placeholder}
+      header={pet.name}
+      meta={`the ${pet.type}`}
+      description={pet.description}
+      extra={pet.owner ? extra : null}
+    />
+  );
+};
--- src/data/pets.js
+++ src/data/pets.js
@@ -4,10 +4,10 @@
   {
     "name": "Your pet's name",
     "owner": "yourGithubUsername",
-    "type": "Which animal, for example: dog, fish, cat",
-    "subtype": "Your pet's breed or an adjective, for example: pug, silly",
+    "type": "Which animal, for example: dog (breed), fish, cat",
     "description": "Anything that describes your buddy. Keep it polite :)",
-    "img": "The URL to a picture of your pet. Make sure it's not too large, 400x400 is a good size"
+    "img": "The URL to a picture of your pet, or null if you don't have one.
+    // Make sure the image is not too large, 400x400 is a good size"
   },
 */
 
@@ -16,15 +16,13 @@ export const pets = [
     "name": "Molly",
     "owner": "vspedr",
     "type": "poodle",
-    "subtype": "fluffy",
     "description": "A cute, but energetic poodle. She's now a little star in a better place.",
-    "img": null
+    "img": 'https://image.ibb.co/dz9Opb/molly.jpg'
   },
   {
     "name": "Oripo",
     "owner": "vspedr",
-    "type": "fish",
-    "subtype": "Betta",
+    "type": "Betta fish",
     "description": "A dark blue, almost black bubble maker. Eats a lot. Sometimes too much.",
     "img": null
   },
