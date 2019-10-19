1a7104d6485a8e9cbc7f7d32e120d2e10ad21e3e
Humano Laranja <contato.carlos@outlook.com>
2019-10-19 12:44:13 -0300

    resolve merge

--- src/services/pets.js
+++ src/services/pets.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
-const fetchPets = async () => {
-  const result = await axios(`${process.env.REACT_APP_API_URL}/pets`);
-
-  return result.data;
+const fetchPets = async (config) => {
+  return await axios(`${process.env.REACT_APP_API_URL}/pets`, config);
 };
 
 export default fetchPets;
