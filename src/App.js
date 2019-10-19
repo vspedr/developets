e05b637084696003faa9ba9404e96b77dd701e25
Fukuda Naoto <never9612@gmail.com>
2019-10-19 14:56:54 +0900

    Add pagenation

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import React, { useState, useEffect } from 'react';
-import { Container, Card, Header, Icon } from 'semantic-ui-react';
+import { Container, Card, Header, Icon, Pagination } from 'semantic-ui-react';
 import 'semantic-ui-css/semantic.min.css';
 import axios from 'axios';
+import parse from 'parse-link-header';
 import './App.css';
 import { PetCard } from './components/PetCard';
 
 function App() {
+  const LIMIT = 9;
+
   const [pets, setPets] = useState([]);
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
 
   useEffect(() => {
     async function fetchData() {
-      const result = await axios(`${process.env.REACT_APP_API_URL}/pets`);
+      const result = await axios(
+        `${process.env.REACT_APP_API_URL}/pets?_page=${page}&_limit=${LIMIT}`
+      );
+      const parsedLink = parse(result.headers.link);
+      setTotalPages(parsedLink.last._page);
       setPets(result.data);
     }
     fetchData();
-  }, []);
+  }, [page]);
 
   return (
     <div className="App">
@@ -29,6 +38,13 @@ function App() {
           <Card.Group stackable>{pets.map(pet => PetCard(pet))}</Card.Group>
         </Container>
       </div>
+      <Pagination
+        activePage={page}
+        onPageChange={(event, data) => {
+          setPage(data.activePage);
+        }}
+        totalPages={totalPages}
+      ></Pagination>
     </div>
   );
 }
