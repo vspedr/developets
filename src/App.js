a1d2c1aedd97a6dd020c1cb82203ef20ad960fa8
dib6596 <cory.harkins@cerecore.net>
2019-10-19 22:00:37 -0500

    Add search functionality and styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React, { useState, useEffect } from 'react';
-import { Container, Card, Header, Icon, Pagination } from 'semantic-ui-react';
-import 'semantic-ui-css/semantic.min.css';
-import parse from 'parse-link-header';
-import './App.css';
-import { PetCard } from './components/PetCard';
-import fetchPets from './services/pets';
+import React, { useState, useEffect } from "react";
+import {
+  Container,
+  Card,
+  Header,
+  Icon,
+  Pagination,
+  Input
+} from "semantic-ui-react";
+import "semantic-ui-css/semantic.min.css";
+import parse from "parse-link-header";
+import "./App.css";
+import { PetCard } from "./components/PetCard";
+import fetchPets from "./services/pets";
 
 function App() {
   const LIMIT = 9;
@@ -12,6 +19,7 @@ function App() {
   const [pets, setPets] = useState([]);
   const [page, setPage] = useState(1);
   const [totalPages, setTotalPages] = useState(1);
+  const [searchText, setSearchText] = useState("");
 
   useEffect(() => {
     async function fetchData() {
@@ -30,6 +38,18 @@ function App() {
     setPage(data.activePage);
   };
 
+  const findPetBy = (pet, _search) =>
+    pet.toLowerCase().includes(_search.toLowerCase());
+
+  const searchPets =
+    pets &&
+    pets.filter(
+      pet =>
+        findPetBy(pet.name, searchText) ||
+        findPetBy(pet.owner, searchText) ||
+        findPetBy(pet.type, searchText)
+    );
+
   return (
     <div className="App">
       <header className="App-header">
@@ -38,9 +58,23 @@ function App() {
           <Header.Content>DeveloPets</Header.Content>
         </Header>
       </header>
+      <div className="App-search">
+        <Header as="h3">
+          <Header.Content>Looking for a specific pet?</Header.Content>
+        </Header>
+        <Input
+          className="App-search-input"
+          icon="search"
+          placeholder="Search by name, owner, or species..."
+          type="text"
+          onChange={e => setSearchText(e.target.value)}
+        />
+      </div>
       <div className="App-content">
         <Container>
-          <Card.Group className="centered" stackable>{pets.map(pet => PetCard(pet))}</Card.Group>
+          <Card.Group className="centered" stackable>
+            {searchPets.map(pet => PetCard(pet))}
+          </Card.Group>
         </Container>
       </div>
       <div className="App-pagination">
